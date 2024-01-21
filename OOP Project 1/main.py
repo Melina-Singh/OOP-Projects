5aab9416499ea582600707b3d7c52a3aa5d13b6b
Melina-Singh <85394677+Melina-Singh@users.noreply.github.com>
2024-01-21 10:36:44 +0545

    oop project day 1

---
+++ OOP Project 1/main.py
@@ -0,0 +1,37 @@
+from menu import Menu, MenuItems
+from coffee import Coffeemaker
+from money_machine import MoneyMachine
+
+items_menu = Menu()
+coffee_machine = Coffeemaker()
+money_machine = MoneyMachine()
+
+# Prompt for the user by asking "What would you like to have"?(espresso/latte/cappuccino)
+def report():
+    coffee_machine.report()
+    money_machine.report()
+# items_menu.get_items()
+
+# user_report = input("If you want the report, type report")
+
+
+machine_ON = True
+while machine_ON:
+    customer_choice = input(f"\n\nType REPORT to see if the ingredients are enpugh." +
+                            f"\n Type OFF if you want to turn the machine off"
+                            f"\n What would you like to have?{items_menu.get_items()}\n").lower()
+    
+    if customer_choice == 'report':
+        report()
+    elif customer_choice == 'off':
+        machine_ON = False
+
+    else:
+        drink = items_menu.find_drink(customer_choice)
+        if coffee_machine.is_resources_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+            coffee_machine.make_coffee(drink)
+        else:
+            break
+
+
---
+++ OOP Project 1/menu.py
@@ -0,0 +1,35 @@
+class MenuItems:
+    # Models each menu item.
+    def __init__(self, name, milk, coffee, water,cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            'water':water,
+            'milk':milk,
+            'coffee':coffee
+        }
+
+class Menu:
+    # Models the menu with drinks. 
+    def __init__(self):
+        self.menu = [
+            MenuItems(name = "lattee",water = 200, milk = 150, coffee = 24, cost = 2.5),
+            MenuItems(name = "espresso",water = 50, milk =0, coffee = 12, cost = 1.5),
+            MenuItems(name = "cappuccino",water = 250, milk = 50, coffee = 24, cost = 3.5)
+        ]
+
+    def get_items(self):
+        # Returns all the names of the available menu items
+        options = " "
+        for item in self.menu:
+            options += f"{item.name} | "
+        return options
+    
+    def find_drink(self,order_name):
+        # searches the menu fo a particular drink by name. Returns the iten if it exists, otherwise returns none.
+
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry that item is not available.")
+ 
